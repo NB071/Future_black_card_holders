80415dcd5b9155215c61aa33603b3f5be929113a
Nima Bargestan <nimam700@gmail.com>
2023-05-04 22:22:52 -0400

    create scss files and icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import "./styles/main.scss";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>H1</h1>
+      <h2>H2</h2>
+      <h3>H3</h3>
+      <p>Body</p>
+    </>
   );
 }
 
